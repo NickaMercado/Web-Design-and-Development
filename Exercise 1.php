d302e13cf245b9140deb07fe58f9e2b2c827092d
Nicka Mercado <90292988+NickaMercado@users.noreply.github.com>
2022-10-01 12:54:50 +0800

    Add files via upload

---
+++ Exercise 1.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<title> Exercise 1 WDD </title>
+</head>
+<body>
+<center>
+
+<?php
+	//declares variable
+	$name = "Nicka Mae S. Mercado";
+	$college_program = "Computer Engineering";
+	$year = "3rd Year";
+	$section = "Wed Design and Development - 58043";
+	$string = "Today, I will learn PHP programming.";
+	
+	//print variables
+	echo "$name";
+	echo "<br>";
+	echo "$college_program";
+	echo "<br>";
+	echo "$year";
+	echo "<br>";
+	echo "$section";
+	echo "<br>";
+	echo $string;
+	echo "<br>";
+	
+?>
+
+
+</body>
+</html>
